910f5d47dc7cca000cd31917bd1222d132d3d017
raghad <raghad.hav@gmail.com>
2020-10-28 15:14:21 +0300

    part-3

---
+++ part-3/The Phonebook/src/services/backend.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+const baseUrl = '/api/persons';//'http://localhost:3003/api/persons';
+
+const getAll = () => {
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
+
+const create = (newObject) => {
+  const request = axios.post(baseUrl, newObject);
+  return request.then((response) => response.data);
+};
+
+const update = (id, updatedObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, updatedObject);
+  return request
+    .then((response) => response.data)
+    .catch((error) => {
+      console.log(`fail`);
+      throw error;
+    });
+};
+const deleteItem = (id) => {
+  const request = axios.delete(`${baseUrl}/${id}`);
+  return request.then((response) => response.data);
+};
+export default {
+  getAll,
+  create,
+  update,
+  deleteItem,
+};
