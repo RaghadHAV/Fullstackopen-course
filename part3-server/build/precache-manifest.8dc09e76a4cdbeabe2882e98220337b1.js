5eb7afd8f8639c339d09e0fed7703f340ee732af
raghad <raghad.hav@gmail.com>
2020-11-09 21:14:48 +0300

    build

---
+++ part3-server/build/precache-manifest.8dc09e76a4cdbeabe2882e98220337b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1a361187f10eed1aa915a3436c6fd54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5eab39cf7991d531e0f9",
+    "url": "/static/css/main.f6465f7e.chunk.css"
+  },
+  {
+    "revision": "d9d1799b4039e2ac7968",
+    "url": "/static/js/2.a3df81c2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a3df81c2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5eab39cf7991d531e0f9",
+    "url": "/static/js/main.beb94716.chunk.js"
+  },
+  {
+    "revision": "efc93394c52cf1132811",
+    "url": "/static/js/runtime-main.9eb0259d.js"
+  }
+]);
